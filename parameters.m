8d62b0b93be0aea94e8aad612c2310c18519eb57
Mikeh <Mikeh@Mike>
2023-09-07 16:59:31 +0200

    Cleaned up the file and added comments

--- parameters.m
+++ parameters.m
@@ -1,22 +1,20 @@
-LR = mean([3880,3850,3920]);
-LF = mean([2855,2873,2875]);
-RF = mean([2985,2972,2725]);
-RR = mean([3814,3834,3909]);
+%% parameter calculations
+LR = mean([3880,3850,3920]); % Weight on left rear tire
+LF = mean([2855,2873,2875]); % Weight on left front tire
+RF = mean([2985,2972,2725]); % Weight on right front tire
+RR = mean([3814,3834,3909]); % Weight on right rear tire
 
-Wheelbase = 53.5/100;
+WeightFront = LF+RF; % weight on the front axle in grams
+WeightRear = LR+RR; % weight on the rear axle in grams
+WeightTotal = WeightRear + WeightFront; % total weight of the car in grams
 
-Trackwidth = 38/100;
+Wheelbase = 53.5/100; % wheelbase in meters
+Trackwidth = 38/100; % trackwidth in meters
+ContactWidth = 5.5/100; % width of contact area in meters
+Wheel_r = 11.3/2/100; % wheelradius in meters
 
-ContactWidth = 5.5/100;
+a = (WeightRear/WeightTotal) * Wheelbase % length from the center of gravity to the front axle
+b = (WeightFront/WeightTotal) * Wheelbase % length from the center of gravity to the rear axle
+Weightdistribution = a/Wheelbase; 
 
-Wheel_r = 11.3/2/100;
-
-WeightFront = LF+RF;
-WeightRear = LR+RR;
-
-WeightTotal = WeightRear + WeightFront;
-
-a = (WeightRear/WeightTotal) * Wheelbase
-b = (WeightFront/WeightTotal) * Wheelbase
-
-Weightdistribution = a/Wheelbase;
+%%
