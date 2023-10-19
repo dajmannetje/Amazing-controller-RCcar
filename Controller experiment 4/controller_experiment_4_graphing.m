2a337d9bfe37934828a5114ba79b7a53befc89b7
Jesper2804 <j.f.bongers@student.tue.nl>
2023-10-19 15:24:20 +0200

    Added experiment 4 data

--- Controller experiment 4/controller_experiment_4_graphing.m
+++ Controller experiment 4/controller_experiment_4_graphing.m
@@ -1,4 +1,4 @@
-%% Experiment controller 4 on car
+%% Experiment controller 4 on car 2
 clear all;
 clc; 
 
@@ -14,10 +14,10 @@ speed_no_controller = transpose([speed_FL_no_controller, speed_FR_no_controller]
 speed_ave_no_controller = mean(speed_no_controller);
 
 %% Load all data of experiments
-for i = 1:2     % Change to number of experiments
+for i = 1:7     % Change to number of experiments
     if i == 1
-        % Kp = 0.3, Ki = 0
-        load('experiment_1.mat')
+        % Kp = 1.5, Ki = 0.05
+        load('experiment_1.mat');
         yawrate_1 = yawrate;
         time_1 = time;
         speed_FL_1 = speed_FL;
@@ -29,7 +29,7 @@ for i = 1:2     % Change to number of experiments
         throttle_out_left_1 = throttle_out_left;
         throttle_out_right_1 = throttle_out_right;
     elseif i == 2
-        % Kp = 1.5, Ki = 0.01
+        % Kp = 1.5, Ki = 0.05
         load('experiment_2.mat')
         yawrate_2 = yawrate;
         time_2 = time;
@@ -41,6 +41,7 @@ for i = 1:2     % Change to number of experiments
         speed_ave_2 = mean(speed_2);
     elseif i == 3
         load('experiment_3.mat')
+        % Kp = 2.5, Ki = 0.175
         yawrate_3 = yawrate;
         time_3 = time;
         speed_FL_3 = speed_FL;
@@ -49,8 +50,11 @@ for i = 1:2     % Change to number of experiments
         speed_RR_3 = speed_RR; 
         speed_3 = transpose([speed_FL_3, speed_FR_3]);
         speed_ave_3 = mean(speed_3);
+        throttle_out_left_3  = throttle_out_left;
+        throttle_out_right_3 = throttle_out_right;
     elseif i == 4
         load('experiment_4.mat') 
+        % Kp = 2.5, Ki = 0.175
         yawrate_4 = yawrate;
         time_4 = time;
         speed_FL_4 = speed_FL;
@@ -61,6 +65,7 @@ for i = 1:2     % Change to number of experiments
         speed_ave_4 = mean(speed_4);
     elseif i == 5
         load('experiment_5.mat')
+         % Kp = 5, Ki = 0.04
         yawrate_5 = yawrate;
         time_5 = time;
         speed_FL_5 = speed_FL;
@@ -71,6 +76,7 @@ for i = 1:2     % Change to number of experiments
         speed_ave_5 = mean(speed_5);
     elseif i == 6
         load('experiment_6.mat')
+        % Kp = 2.5, Ki = 0.35
         yawrate_6 = yawrate;
         time_6 = time;
         speed_FL_6 = speed_FL;
@@ -81,6 +87,7 @@ for i = 1:2     % Change to number of experiments
         speed_ave_6 = mean(speed_6);
     elseif i == 7
         load('experiment_7.mat') 
+        % Kp = 3.5, Ki = 0.35
         yawrate_7 = yawrate;
         time_7 = time;
         speed_FL_7 = speed_FL;
@@ -103,54 +110,69 @@ for i = 1:2     % Change to number of experiments
 end
 
 
-%% Plotting
-color = [199, 25, 24]/256;
-
-% No controller, experiment a
-figure(1)
-scatter(abs(speed_ave_no_controller), abs(yawrate_no_controller), 'b.');
-hold on 
-grid on
-scatter(abs(speed_ave_1), abs(yawrate_1), 'r.');
-xlim([0 4])
-ylim([0 2.5])
-title('No controller, experiment a', 'Interpreter', 'latex')
-xlabel('Velocity (m/s)', 'Interpreter', 'latex') 
-ylabel('Yaw Rate (rad/s)', 'Interpreter', 'latex') 
-legend({'No controller','experiment a', 'model'},'Location','northwest', 'Interpreter', 'latex')
-
-% No controller, experiment b
-figure(2)
-scatter(abs(speed_ave_no_controller), abs(yawrate_no_controller), 'b.');
-hold on 
-grid on
-scatter(abs(speed_ave_2), abs(yawrate_2), 'r.');
-xlim([0 4])
-ylim([0 2.5])
-title('No controller, experiment b', 'Interpreter', 'latex')
-xlabel('Velocity (m/s)', 'Interpreter', 'latex') 
-ylabel('Yaw Rate (rad/s)', 'Interpreter', 'latex') 
-legend({'No controller','experiment b', 'model'},'Location','northwest', 'Interpreter', 'latex')
-
-% experiment a, experiment b
-figure(3)
-scatter(abs(speed_ave_1), abs(yawrate_1), 'b.');
-hold on 
-grid on
-scatter(abs(speed_ave_2), abs(yawrate_2), 'r.');
-xlim([0 4])
-ylim([0 2.5])
-title('experiment a, experiment b', 'Interpreter', 'latex')
-xlabel('Velocity (m/s)', 'Interpreter', 'latex') 
-ylabel('Yaw Rate (rad/s)', 'Interpreter', 'latex') 
-legend({'experiment a','experiment b', 'model'},'Location','northwest', 'Interpreter', 'latex')
-
-
+% %% Plotting
+% color = [199, 25, 24]/256;
+% 
+% % No controller, experiment a
+% figure(1)
+% scatter(abs(speed_ave_no_controller), abs(yawrate_no_controller), 'b.');
+% hold on 
+% grid on
+% scatter(abs(speed_ave_1), abs(yawrate_1), 'r.');
+% xlim([0 4])
+% ylim([0 2.5])
+% title('No controller, experiment a', 'Interpreter', 'latex')
+% xlabel('Velocity (m/s)', 'Interpreter', 'latex') 
+% ylabel('Yaw Rate (rad/s)', 'Interpreter', 'latex') 
+% legend({'No controller','experiment a', 'model'},'Location','northwest', 'Interpreter', 'latex')
+% 
+% % No controller, experiment b
+% figure(2)
+% scatter(abs(speed_ave_no_controller), abs(yawrate_no_controller), 'b.');
+% hold on 
+% grid on
+% scatter(abs(speed_ave_2), abs(yawrate_2), 'r.');
+% xlim([0 4])
+% ylim([0 2.5])
+% title('No controller, experiment b', 'Interpreter', 'latex')
+% xlabel('Velocity (m/s)', 'Interpreter', 'latex') 
+% ylabel('Yaw Rate (rad/s)', 'Interpreter', 'latex') 
+% legend({'No controller','experiment b', 'model'},'Location','northwest', 'Interpreter', 'latex')
+% 
+% % experiment a, experiment b
+% figure(3)
+% scatter(abs(speed_ave_1), abs(yawrate_1), 'b.');
+% hold on 
+% grid on
+% scatter(abs(speed_ave_2), abs(yawrate_2), 'r.');
+% xlim([0 4])
+% ylim([0 2.5])
+% title('experiment a, experiment b', 'Interpreter', 'latex')
+% xlabel('Velocity (m/s)', 'Interpreter', 'latex') 
+% ylabel('Yaw Rate (rad/s)', 'Interpreter', 'latex') 
+% legend({'experiment a','experiment b', 'model'},'Location','northwest', 'Interpreter', 'latex')
+% 
+% 
+% 
+% % experiment 1, experiment 3
 % figure(4)
+% scatter(abs(speed_ave_1), abs(yawrate_1), 'b.');
+% hold on 
+% grid on
+% scatter(abs(speed_ave_3), abs(yawrate_3), 'r.');
+% xlim([0 4])
+% ylim([0 2.5])
+% title('experiment a, experiment b', 'Interpreter', 'latex')
+% xlabel('Velocity (m/s)', 'Interpreter', 'latex') 
+% ylabel('Yaw Rate (rad/s)', 'Interpreter', 'latex') 
+% legend({'experiment a','experiment b', 'model'},'Location','northwest', 'Interpreter', 'latex')
+% 
+% 
+% figure(5)
 % hold on
 % grid on
-% plot(time_1,throttle_out_left_1, "b");
-% plot(time_1,throttle_out_right_1, "r");
+% plot(time_1,throttle_out_left_3, "b");
+% plot(time_1,throttle_out_right_3, "r");
 % xlim([7 25]);
 % ylim([53 62]);
 % title('Test figure (replace in final report)', 'Interpreter', 'latex')
@@ -158,5 +180,42 @@ legend({'experiment a','experiment b', 'model'},'Location','northwest', 'Interpr
 % ylabel('Throttle [\%]', 'Interpreter', 'latex') 
 % legend({'Throttle left','Throttle right'},'Location','northeast', 'Interpreter', 'latex')
 
+
+
+
+
+
+figure(2)
+scatter(abs(speed_ave_no_controller), abs(yawrate_no_controller), 'b.');
+hold on 
+grid on
+scatter(abs(speed_ave_1), abs(yawrate_1), '.');
+scatter(abs(speed_ave_2), abs(yawrate_2), '.');
+scatter(abs(speed_ave_3), abs(yawrate_3), '.');
+scatter(abs(speed_ave_4), abs(yawrate_4), '.');
+scatter(abs(speed_ave_5), abs(yawrate_5), '.');
+scatter(abs(speed_ave_6), abs(yawrate_6), '.');
+scatter(abs(speed_ave_7), abs(yawrate_7), '.');
+xlim([0 4])
+ylim([0 2.5])
+title('No controller, experiment b', 'Interpreter', 'latex')
+xlabel('Velocity (m/s)', 'Interpreter', 'latex') 
+ylabel('Yaw Rate (rad/s)', 'Interpreter', 'latex') 
+legend({'No controller','experiment 1', 'experiment 2', 'experiment 3', 'experiment 4', 'experiment 5', 'experiment 6', 'experiment 7'},'Location','northwest', 'Interpreter', 'latex', FontSize=15)
+
+
+
+figure(3)
+scatter(abs(speed_ave_no_controller), abs(yawrate_no_controller), 'b.');
+hold on 
+grid on
+scatter(abs(speed_ave_4), abs(yawrate_4), 'r.');
+scatter(abs(speed_ave_5), abs(yawrate_5), 'g.');
+xlim([0 4])
+ylim([0 2.5])
+title('No controller, experiment a', 'Interpreter', 'latex')
+xlabel('Velocity (m/s)', 'Interpreter', 'latex') 
+ylabel('Yaw Rate (rad/s)', 'Interpreter', 'latex') 
+legend({'No controller','experiment 4', 'experiment 5'},'Location','northwest', 'Interpreter', 'latex')
 
 
